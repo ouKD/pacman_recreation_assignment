49c3689f7e5eada4afa736109be427c14f710cf5
ouKD <111720334+ouKD@users.noreply.github.com>
2023-10-31 17:57:23 +1100

    PacStudent Movement 2
    Added tween list and tweener script to use for lerping script for PacStudentController.
    merged missing dev-branch latest origin into Feature-Movement

--- PacCreatorProject/Assets/Scripts/PacStudentController.cs
+++ PacCreatorProject/Assets/Scripts/PacStudentController.cs
@@ -5,31 +5,32 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     string lastInput;
+    private Tweener tweener;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tweener = GetComponent<Tweener>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("W"))
+        if (Input.GetKeyDown("w"))
         {
             
         }
         
-         if (Input.GetKeyDown("A"))
+         if (Input.GetKeyDown("a"))
         {
             
         }
 
-         if (Input.GetKeyDown("S"))
+         if (Input.GetKeyDown("s"))
         {
             
         }
 
-         if (Input.GetKeyDown("D"))
+         if (Input.GetKeyDown("d"))
         {
             
         }
