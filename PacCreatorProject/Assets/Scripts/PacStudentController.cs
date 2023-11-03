37709308a99c9ce0df63872668d65f5b67d6892a
ouKD <111720334+ouKD@users.noreply.github.com>
2023-11-03 18:56:58 +1100

    PacStudent Movement 1
    created animation controller and animator for pacstudent
    added params for all animation controllers

--- PacCreatorProject/Assets/Scripts/PacStudentController.cs
+++ PacCreatorProject/Assets/Scripts/PacStudentController.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PacStudentController : MonoBehaviour
 {
+    string lastInput;
+    private Tweener tweener;
+    public Animator pacMovement;
     // Start is called before the first frame update
     void Start()
     {
+        tweener = GetComponent<Tweener>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("w")){
+            pacMovement.SetTrigger("UpParam");
+
+        }
         
+         if (Input.GetKeyDown("a")){
+            pacMovement.SetTrigger("LeftParam");
+            
+        }
+
+         if (Input.GetKeyDown("s")){
+            pacMovement.SetTrigger("DownParam");
+        }
+
+         if (Input.GetKeyDown("d")){
+            pacMovement.SetTrigger("RightParam");
+        }
+
+
     }
 }
